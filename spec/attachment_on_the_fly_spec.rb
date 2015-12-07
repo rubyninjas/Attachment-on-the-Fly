0fc6ad0d72b009806b4b1a431f748c78ede2da88
twonegatives <whitewhiteheaven@gmail.com>
2015-12-07 23:52:26 +0300

    deprecated rspec .should syntax changed to expect()

--- spec/attachment_on_the_fly_spec.rb
+++ spec/attachment_on_the_fly_spec.rb
@@ -16,12 +16,12 @@ describe "Attachment on the fly mixin" do
     }
 
     method_names.each do |method_name|
-      it { should respond_to(method_name.to_sym) }
+      it { is_expected.to respond_to(method_name.to_sym) }
     end
 
-    it { should_not respond_to(:x_125_250) }
-    it { should_not respond_to(:s_125_250foo) }
-    it { should_not respond_to(:S_125_250) }
+    it { is_expected.not_to respond_to(:x_125_250) }
+    it { is_expected.not_to respond_to(:s_125_250foo) }
+    it { is_expected.not_to respond_to(:S_125_250) }
   end
 
   context "#method_missing" do
@@ -37,13 +37,13 @@ describe "Attachment on the fly mixin" do
 
       method_name_to_generate_image_call.each do |method_name, generate_image_args|
         it "#{method_name}" do
-          subject.should_receive(:generate_image).with(*generate_image_args)
+          expect(subject).to receive(:generate_image).with(*generate_image_args)
           subject.send(method_name)
         end
       end
 
       it "passes parameters through as well" do
-        subject.should_receive(:generate_image).with("width", 125, 125, {:quality => 90, :extension => "jpeg", :colorspace => "sRGB"})
+        expect(subject).to receive(:generate_image).with("width", 125, 125, {:quality => 90, :extension => "jpeg", :colorspace => "sRGB"})
         subject.s_125_width :quality => 90, :extension => "jpeg", :colorspace => "sRGB"
       end
     end
@@ -51,7 +51,7 @@ describe "Attachment on the fly mixin" do
 
   context "#generate_image" do
 
-    context "it should generate a new image" do
+    context "it generates a new image" do
       method_name_to_expectations = {
         :s_125_width => {
           :new => "/S_125_WIDTH__q_100__path.png",
@@ -68,41 +68,41 @@ describe "Attachment on the fly mixin" do
       }
       method_name_to_expectations.each do |method_name, expected|
         it "for #{method_name}" do
-          File.should_receive(:exist?).with(expected[:new]).and_return(false)
-          File.should_receive(:exist?).with("//file.png").and_return(true)
-          subject.should_receive(:convert_file!)
+          expect(File).to receive(:exist?).with(expected[:new]).and_return(false)
+          expect(File).to receive(:exist?).with("//file.png").and_return(true)
+          expect(subject).to receive(:convert_file!)
           subject.send(method_name)
         end
       end
 
       it "passes in parameters for quality" do
-        File.should_receive(:exist?).with("/S_125_WIDTH__q_75__path.png").and_return(false)
-        File.should_receive(:exist?).with("//file.png").and_return(true)
-        subject.should_receive(:convert_file!)
+        expect(File).to receive(:exist?).with("/S_125_WIDTH__q_75__path.png").and_return(false)
+        expect(File).to receive(:exist?).with("//file.png").and_return(true)
+        expect(subject).to receive(:convert_file!)
         subject.s_125_width :quality => 75
       end
 
       it "passes in parameters for extension" do
-        File.should_receive(:exist?).with("/S_125_WIDTH_extension_jpeg_q_75__path.jpeg").and_return(false)
-        File.should_receive(:exist?).with("//file.png").and_return(true)
-        subject.should_receive(:convert_file!)
-        subject.should_receive(:has_alpha?).with("//file.png").and_return(false)
-        subject.s_125_width(:quality => 75, :extension => "jpeg").should == "/S_125_WIDTH_extension_jpeg_q_75__path.jpeg"
+        expect(File).to receive(:exist?).with("/S_125_WIDTH_extension_jpeg_q_75__path.jpeg").and_return(false)
+        expect(File).to receive(:exist?).with("//file.png").and_return(true)
+        expect(subject).to receive(:convert_file!)
+        expect(subject).to receive(:has_alpha?).with("//file.png").and_return(false)
+        expect(subject.s_125_width(:quality => 75, :extension => "jpeg")).to eq("/S_125_WIDTH_extension_jpeg_q_75__path.jpeg")
       end
 
       it "preserves original extension if file has alpha channel" do
-        File.should_receive(:exist?).with("/S_125_WIDTH_extension_jpeg_q_75__path.png").and_return(false)
-        File.should_receive(:exist?).with("//file.png").and_return(true)
-        subject.should_receive(:convert_file!)
-        subject.should_receive(:has_alpha?).with("//file.png").and_return(true)
-        subject.s_125_width(:quality => 75, :extension => "jpeg").should == "/S_125_WIDTH_extension_jpeg_q_75__path.png"
+        expect(File).to receive(:exist?).with("/S_125_WIDTH_extension_jpeg_q_75__path.png").and_return(false)
+        expect(File).to receive(:exist?).with("//file.png").and_return(true)
+        expect(subject).to receive(:convert_file!)
+        expect(subject).to receive(:has_alpha?).with("//file.png").and_return(true)
+        expect(subject.s_125_width(:quality => 75, :extension => "jpeg")).to eq("/S_125_WIDTH_extension_jpeg_q_75__path.png")
       end
 
       it "passes in parameters for colorspace" do
-        File.should_receive(:exist?).with("/S_125_WIDTH_colorspace_sRGB_q_100__path.png").and_return(false)
-        File.should_receive(:exist?).with("//file.png").and_return(true)
-        subject.should_receive(:convert_file!)
-        subject.s_125_width(:colorspace => "sRGB").should == "/S_125_WIDTH_colorspace_sRGB_q_100__path.png"
+        expect(File).to receive(:exist?).with("/S_125_WIDTH_colorspace_sRGB_q_100__path.png").and_return(false)
+        expect(File).to receive(:exist?).with("//file.png").and_return(true)
+        expect(subject).to receive(:convert_file!)
+        expect(subject.s_125_width(:colorspace => "sRGB")).to eq("/S_125_WIDTH_colorspace_sRGB_q_100__path.png")
       end
     end
   end
@@ -136,7 +136,7 @@ describe "Attachment on the fly mixin" do
       expect(subject).to receive(:upload_converted_file)
       expect(File).to receive(:delete).twice
       #expect(subject).to receive(:remove_local_files)
-      subject.s_125_width.should == "/S_125_WIDTH__q_100__path.png"
+      expect(subject.s_125_width).to eq("/S_125_WIDTH__q_100__path.png")
     end
 
     it "returns url instantly if resized version exists" do
@@ -144,7 +144,7 @@ describe "Attachment on the fly mixin" do
       expect(subject).not_to receive(:original_file_exist?)
       expect(subject).not_to receive(:convert_file!)
       expect(subject).not_to receive(:execute_command!)
-      subject.s_125_width.should == "/S_125_WIDTH__q_100__path.png"
+      expect(subject.s_125_width).to eq("/S_125_WIDTH__q_100__path.png")
     end
   end
 
